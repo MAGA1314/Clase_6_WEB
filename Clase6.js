443b93077771d372fe3b0d53869b5277061ca241
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-03-13 13:15:21 -0500

    clase con datos privados

--- Clase6.js
+++ Clase6.js
@@ -1,11 +1,19 @@
 // Clases
+// clase publica y ´privada
 class silla{
     // Constructor
+    #precioCompraDistribuidor;
+    #maximoDescuento;
+    #nombreVendedor;
+
     constructor(nombre, color, precio, modelo){
         this.nombre = nombre;
         this.color = color;
         this.precio = precio;
         this.modelo = modelo;
+        this.#precioCompraDistribuidor = 1200;
+        this.#maximoDescuento = 0.3;
+        this.#nombreVendedor = "juan";
     }
     cambiarColor(nuevoColor){
         this.color = "Negro";
@@ -16,23 +24,37 @@ class silla{
     cambiarModelo(nuevoModelo){
         this.modelo = nuevoModelo;
     }
+    cambiarNombreVendedor(nombreVendedor){
+        this.#nombreVendedor = nombreVendedor;
+    }
+    mostrarvendedor(){
+        console.log(this.#nombreVendedor);
+    }
 }
 silla.prototype.mostrarprecio = function(){
     console.log(this.precio);
 }
+
 const sillaCasa = new silla("Rimax", "Blanco", 320, "ZXX300")
 console.log(sillaCasa);
 sillaCasa.mostrarprecio();
 
+
 // Herencia
 class sillaGamer extends silla{
-    constructor(nombre, color, precio, modelo, tamaño, importada, diseño){
+    constructor(nombre, color, precio, modelo, tamaño, importada, diseño, material){
         super(nombre, color, precio, modelo);
         this.tamaño = tamaño;
         this.importada = importada;
         this.diseño = diseño;
+        this.material = material;
+
     }
 }
+
 console.log(sillaGamer);
-const nuevaSillaGamer = new sillaGamer("Sublime", "Rojo", 520, "RX40",50, true, "confort")
+const nuevaSillaGamer = new sillaGamer("Sublime", "Rojo", 520, "RX40",50, true, "confort", "cuero");
 console.log(nuevaSillaGamer);
+
+console.log(sillaCasa.mostrarvendedor());
+console.log(nuevaSillaGamer.mostrarvendedor());
